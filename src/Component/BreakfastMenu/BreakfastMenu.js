7cbfea0e474472dabe16790d29e189bcce2763e0
teregonzalez <teregonzalez@gmail.com>
2022-05-04 22:48:20 -0400

    Se limpia el archivo, cambios en menu Breakfasy

--- src/Component/BreakfastMenu/BreakfastMenu.js
+++ src/Component/BreakfastMenu/BreakfastMenu.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import { getMenu } from '../../firebase/data-firebase'
+import React, {useEffect, useState} from 'react'
+import {getMenu} from '../../firebase/data-firebase'
 
 
 export const BreakfastMenu = () => {
 
-  const getBreakfastMenu = () => {
-    getMenu()
-    .then(res => console.log(res.breakfast))
-    .catch(error => console.log(error));
-  }
+    const [foods, setFoods] = useState()
 
-  return (
-    <div>
-    <button onClick={ getBreakfastMenu } className="btn btn-light"> DESAYUNO </button>
-    </div>
-  )
+    const getBreakfastMenu = () => {
+        getMenu().then(res => {
+            const menuFoods = Object.keys(res.breakfast);
+            setFoods(menuFoods)
+        }).catch(error => console.log(error));
+    }
+
+    useEffect(() => {
+        getBreakfastMenu()
+    }, [])
+    return (
+        <div>
+            <button onClick={getBreakfastMenu}
+                className="btn btn-light">
+                DESAYUNO
+            </button>
+            {foods && foods.map((food, i) => (
+                    <button key={i}>{ food }</button>
+                ))}
+        </div>
+    )
 }
