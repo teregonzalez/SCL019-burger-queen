85b2363c8bb99784a751ee729bb2d04dc2111099
teregonzalez <teregonzalez@gmail.com>
2022-05-07 23:32:03 -0400

    Cambios css

--- src/Component/AddOrder/AddOrder.js
+++ src/Component/AddOrder/AddOrder.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import {Header} from '../Header/Header'
+import { Header } from '../Header/Header'
 import img404 from '../../img/img404.png';
 import './AddOrder.css'
 
 export const AddOrder = () => {
     return (
+      <>
+      <Header />
         <div className='addContainer'>
-            <Header/>
             <h1>AGREGA TU ORDEN EN CONSTRUCCION</h1>
             <img className="img404"
                 src={img404}
                 alt="img404"/>
-            <Link className="btn" to='/'>VOLVER A HOME</Link>
+            <Link className="btnReturn" to='/edit'>EDITAR PEDIDO</Link>
+            <Link className="btnReturn" to='/status'>ESTADO PEDIDOS</Link>
+            <Link className="btnReturn" to='/'>VOLVER A HOME</Link>
         </div>
+      </>
     )
 }
--- src/Component/OrderStatus/OrderStatus.js
+++ src/Component/OrderStatus/OrderStatus.js
@@ -7,13 +7,17 @@ import './OrderStatus.css'
 
 export const OrderStatus = () => {
     return (
-        <div className='statsContainer'>
+        <>
             <Header/>
-            <h1>ESTATUS DE ORDENES EN CONSTRUCCION</h1>
-            <img className="img404"
-                src={img404}
-                alt="img404"/>
-            <Link className="btn" to='/'>VOLVER A HOME</Link>
-        </div>
+            <div className='statsContainer'>
+                <h1>ESTATUS DE ORDENES EN CONSTRUCCION</h1>
+                <img className="img404"
+                    src={img404}
+                    alt="img404"/>
+                <Link className="btnReturn" to='/add'>AGREGAR PEDIDO</Link>
+                <Link className="btnReturn" to='/edit'>EDITAR PEDIDO</Link>
+                <Link className="btnReturn" to='/'>VOLVER A HOME</Link>
+            </div>
+        </>
     )
 }
