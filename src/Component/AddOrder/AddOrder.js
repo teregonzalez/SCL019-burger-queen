618dc45b74514bc03415761bb14902c151daf4b8
teregonzalez <teregonzalez@gmail.com>
2022-05-07 23:58:27 -0400

    Cambios en AddOrder

--- src/Component/AddOrder/AddOrder.js
+++ src/Component/AddOrder/AddOrder.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-import { Header } from '../Header/Header'
+import {Header} from '../Header/Header'
 import img404 from '../../img/img404.png';
 import './AddOrder.css'
 
 export const AddOrder = () => {
     return (
-      <>
-      <Header />
-        <div className='addContainer'>
-            <h1>AGREGA TU ORDEN EN CONSTRUCCION</h1>
-            <img className="img404"
-                src={img404}
-                alt="img404"/>
-            <Link className="btnReturn" to='/edit'>EDITAR PEDIDO</Link>
-            <Link className="btnReturn" to='/status'>ESTADO PEDIDOS</Link>
-            <Link className="btnReturn" to='/'>VOLVER A HOME</Link>
-        </div>
-      </>
+        <>
+            <Header/>
+            <div className='addContainer'>
+                <p className='titleAdd'>AGREGAR PEDIDO</p>
+                <div className='formContainer'>
+                <p className='titleEditor'>EDITOR DE ORDENES EN CONSTRUCCION</p>
+                    <img className="img404"
+                        src={img404}
+                        alt="img404"/>
+                </div>
+                <Link className="btnReturn" to='/edit'>EDITAR PEDIDO</Link>
+                <Link className="btnReturn" to='/status'>ESTADO PEDIDOS</Link>
+                <Link className="btnReturn" to='/'>VOLVER A HOME</Link>
+            </div>
+        </>
     )
 }
