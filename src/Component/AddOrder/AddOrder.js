5a168091a0115cfb30e33cefa6f55f3d577754c4
teregonzalez <teregonzalez@gmail.com>
2022-05-08 16:51:08 -0400

    Se agrega useState a AddOrder

--- src/Component/AddOrder/AddOrder.js
+++ src/Component/AddOrder/AddOrder.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
 import {Header} from '../Header/Header'
 
 import './AddOrder.css'
 
-const clientName = '';
-
 export const AddOrder = () => {
+
+    const [dataClient, setDataClient] = useState({
+        clientName: '',
+        orderNumber: ''
+    });
+    
+    const handleInputChange = (event) => {
+        console.log(event.target.value);
+        setDataClient({
+            ...dataClient,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    const sendData = (event) => {
+        event.preventDefault();
+        console.log(dataClient.clientName);
+    }
     return (
         <>
             <Header/>
             <div className='addContainer'>
                 <p className='titleAdd'>AGREGAR PEDIDO</p>
-                <form className='formContainer'>
+                <form className='formContainer' onSubmit={ sendData }>
                 <p className='addName'>INGRESAR NOMBRE CLIENTE</p>
-                <input type='text' className='clientName' value={ clientName } placeholder='NOMBRE CLIENTE'></input>
+                <input type='text' 
+                className='clientName' 
+                name='clientName' 
+                placeholder='NOMBRE CLIENTE'
+                onChange={ handleInputChange }></input>
                 <p className='selectTitle'>SELECCIONA MENU</p>
                 <button className='btnMenuBreakfast'>DESAYUNO</button>
                 <button className='btnMenuTraditional'>TRADITIONAL</button>
