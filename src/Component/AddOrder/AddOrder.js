411bb5df26bfa5f7996aaf3b8e1aaa6c5213dd77
teregonzalez <teregonzalez@gmail.com>
2022-05-09 22:50:37 -0400

    Se actualiza vista AddOrder

--- src/Component/AddOrder/AddOrder.js
+++ src/Component/AddOrder/AddOrder.js
@@ -1,45 +1,84 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 
+import {getMenu} from '../../firebase/data-firebase'
 import {Header} from '../Header/Header'
 
 import './AddOrder.css'
 
 export const AddOrder = () => {
 
-    const [dataClient, setDataClient] = useState({
-        clientName: '',
-        orderNumber: ''
-    });
-    
-    const handleInputChange = (event) => {
-        console.log(event.target.value);
-        setDataClient({
-            ...dataClient,
-            [event.target.name] : event.target.value
-        })
+    const [foods, setFoods] = useState();
+    const [str, setStr] = useState('breakfast');
+
+    const toggleMenu = (i) => {
+        setStr(i);
     }
 
-    const sendData = (event) => {
-        event.preventDefault();
-        console.log(dataClient.clientName);
-    }
+    useEffect(() => {
+        getMenu().then(res => {
+            if(str === 'breakfast'){
+                const menuFoods = Object.entries(res.breakfast).map((value) => {
+                    return value
+                  });
+                  setFoods(menuFoods)
+            } if(str === 'traditional'){
+                const menuFoods = Object.entries(res.traditional).map((value) => {
+                    return value
+                  });
+                  setFoods(menuFoods)
+            }
+        }).catch(error => console.log(error));
+    }, [str])
+
+    // const [dataClient, setDataClient] = useState({
+    //     clientName: '',
+    //     orderNumber: ''
+    // });
+    
+    // const handleInputChange = (event) => {
+    //     console.log(event.target.value);
+    //     setDataClient({
+    //         ...dataClient,
+    //         [event.target.name] : event.target.value
+    //     })
+    // }
+
+    // const sendData = (event) => {
+    //     event.preventDefault();
+    //     console.log(dataClient.clientName);
+    // }
     return (
         <>
             <Header/>
             <div className='addContainer'>
                 <p className='titleAdd'>AGREGAR PEDIDO</p>
-                <form className='formContainer' onSubmit={ sendData }>
-                <p className='addName'>INGRESAR NOMBRE CLIENTE</p>
-                <input type='text' 
+                <div className='cartContainer'>
+                <div className='btnMenuContainer'>
+            <button onClick={ () => toggleMenu( 'breakfast' ) }
+                className="btnMenu">
+                DESAYUNO
+            </button>
+
+            <button onClick={ () => toggleMenu( 'traditional' ) }
+                className="btnMenu">
+                TRADITIONAL
+            </button>
+                </div>
+            { foods && foods.map((food, i) => (
+                <>
+                    <button key={i}>{ food[1].name }</button>
+                </>
+                ))}
+                {/* <input type='text' 
                 className='clientName' 
                 name='clientName' 
                 placeholder='NOMBRE CLIENTE'
-                onChange={ handleInputChange }></input>
-                <p className='selectTitle'>SELECCIONA MENU</p>
+                onChange={ handleInputChange }></input> */}
+                {/* <p className='selectTitle'>SELECCIONA MENU</p>
                 <button className='btnMenuBreakfast'>DESAYUNO</button>
-                <button className='btnMenuTraditional'>TRADITIONAL</button>
-                </form>
+                <button className='btnMenuTraditional'>TRADITIONAL</button> */}
+                </div>
                 <Link className="btnReturn" to='/edit'>EDITAR PEDIDO</Link>
                 <Link className="btnReturn" to='/status'>ESTADO PEDIDOS</Link>
                 <Link className="btnReturn" to='/'>VOLVER A HOME</Link>
--- src/Component/BreakfastMenu/BreakfastMenu.js
+++ src/Component/BreakfastMenu/BreakfastMenu.js
@@ -1,36 +1,36 @@
-import React, {useEffect, useState} from 'react'
-import {getMenu} from '../../firebase/data-firebase'
+// import React, {useEffect, useState} from 'react'
+// import {getMenu} from '../../firebase/data-firebase'
 
 
-export const BreakfastMenu = () => {
+// export const BreakfastMenu = () => {
 
-    const [foods, setFoods] = useState();
+//     const [foods, setFoods] = useState();
 
-    const getBreakfastMenu = () => {
-        getMenu().then(res => {
-            const menuFoods = Object.entries(res.breakfast).map((value) => {
-                return value
-              });
-            setFoods(menuFoods)
-        }).catch(error => console.log(error));
-    }
+//     const getBreakfastMenu = () => {
+//         getMenu().then(res => {
+//             const menuFoods = Object.entries(res.breakfast).map((value) => {
+//                 return value
+//               });
+//             setFoods(menuFoods)
+//         }).catch(error => console.log(error));
+//     }
 
-    useEffect(() => {
-        getBreakfastMenu()
-    }, [])
+//     useEffect(() => {
+//         getBreakfastMenu()
+//     }, [])
     
-    return (
-        <div>
-            <button onClick={ getBreakfastMenu }
-                className="btn btn-light">
-                DESAYUNO
-            </button>
-            { foods && foods.map((food, i) => (
-                <>
-                    <button key={i}>{ food[1].name }</button>
-                    <p key={i}> $ { food[1].price }</p>
-                </>
-                ))}
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <button onClick={ getBreakfastMenu }
+//                 className="btn btn-light">
+//                 DESAYUNO
+//             </button>
+//             { foods && foods.map((food, i) => (
+//                 <>
+//                     <button key={i}>{ food[1].name }</button>
+//                     <p key={i}> $ { food[1].price }</p>
+//                 </>
+//                 ))}
+//         </div>
+//     )
+// }
